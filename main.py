32fc1e3962385940d03eae5ce7511a4d7f3b86ce
Diego Facundo Pérez Nicolau <3607834730101@ingenieria.usac.edu.gt>
2025-02-17 20:07:01 -0600

    Created main.py with the POST /song/ function

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+class Song(BaseModel):
+    name: str
+    author: str
+    album: str
+
+songs = []
+
+@app.post("/songs/")
+def create_song(song: Song):
+    songs.append(song)
+    return {  "message": "Song created successfully", "song": song.dict() }
+
